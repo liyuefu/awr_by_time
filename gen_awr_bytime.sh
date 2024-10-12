47d811f7876e3bf2c3e5a47fbab2baad8fcdde21
liyuefu <32407411+liyuefu@users.noreply.github.com>
2024-10-12 11:47:41 +1100

    Update gen_awr_bytime.sh
    change to today

--- gen_awr_bytime.sh
+++ gen_awr_bytime.sh
@@ -6,7 +6,7 @@ AWR_PATH=~/scripts/awr
 #AWR_REPORT_PATH=~/awrrpt
 cd $AWR_PATH
 
-TODAY=`date -d yesterday +%Y%m%d`
+TODAY=`date  +%Y%m%d`
 BEGIN_TIME=${TODAY}$1
 END_TIME=${TODAY}$2
 sqlplus / as sysdba @gen_batch_quiet.sql  $BEGIN_TIME $END_TIME
